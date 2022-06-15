7da5155fb68953b6141020ef921b41f505f4e47b
Haris Iftikhar <59412623+haris-bit@users.noreply.github.com>
2022-06-15 15:14:14 +0500

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -3,16 +3,32 @@ import random
 guess = 0
 
 num = random.randint(1, 100)    
-guess = int(input("Enter a number between 1-100: "))
 
-while guess!=num:
+while True:
+    guess = int(input("Enter a number between 1-100: "))
     if guess==num:
         print("Congrats! You Won!")
         break
     elif guess>num:
         print("Try a Smaller Number")
-        guess = int(input("Enter a number between 1-100: "))
     elif guess<num:
         print("Try a Bigger Number")
-        guess = int(input("Enter a number between 1-100: "))
 
+
+# import random 
+
+# guess = 0
+
+# num = random.randint(1, 100)    
+# guess = int(input("Enter a number between 1-100: "))
+
+# while guess!=num:
+#     if guess==num:
+#         print("Congrats! You Won!")
+#         break
+#     elif guess>num:
+#         print("Try a Smaller Number")
+#         guess = int(input("Enter a number between 1-100: "))
+#     elif guess<num:
+#         print("Try a Bigger Number")
+#         guess = int(input("Enter a number between 1-100: "))
